e2eb0815ddc0fcad07b514063d063a3994d18cc7
Helmy Ezzat <hellmyezzat@gmail.com>
2024-02-16 15:25:58 +0200

    added decorative circles,and added data (icons - images - and expanded skills list. )  and install react-icons

--- app/layout.tsx
+++ app/layout.tsx
@@ -16,7 +16,23 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={`${inter.className} bg-gray-50 text-gray-950 relative`}>
+        <div
+          className="
+        bg-[#fbe2e3] w-[31.25rem] h-[31.25rem] rounded-full
+          absolute top-[-6rem] right-[11rem] -z-10
+          sm:w-[68.75rem]
+          blur-[11rem]"
+        />
+        <div
+          className="
+        bg-[#dbd7fb] w-[50rem] h-[31.25rem] rounded-full
+          absolute top-[-1rem] left-[-35rem] -z-10
+          sm:w-[68.75rem] md:left-[-33rem] lg:left-[-28rem] xl:left-[-15rem] 2xl:left-[-5rem]
+          blur-[11rem]"
+        />
+        {children}
+      </body>
     </html>
   )
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
-    <main className="bg-slate-700 text-white h-svh flex justify-center items-center">
-      <h1 className="text-6xl font-bold">Helmy`s Portfolio</h1>
+    <main>
+      <h1>Helmy`s Portfolio</h1>
     </main>
   )
 }
