4ef81ffe830fb3366099c475d9cfa3099e57ab67
Helmy Ezzat <hellmyezzat@gmail.com>
2024-02-19 07:23:27 +0200

    "Added state variable timeOfLastClick to keep track of the time of the last click event. This is utilized for temporarily disabling the observer when a user clicks on a link."

--- context/active-section-context.tsx
+++ context/active-section-context.tsx
@@ -9,21 +9,27 @@ type SectionName = (typeof links)[number]['name']
 type activeType = {
   activeSection: SectionName
   setActiveSection: React.Dispatch<React.SetStateAction<SectionName>>
+  timeOfLastClick: number
+  setTimeOfLastClick: React.Dispatch<React.SetStateAction<number>>
 }
 
-export const ActiveSlice =
-  createContext<activeType | null>(null)
+export const ActiveSlice = createContext<activeType | null>(null)
 
-function ActiveSectionContextProvider({
-  children,
-}: propsType) {
+function ActiveSectionContextProvider({ children }: propsType) {
   const [activeSection, setActiveSection] = useState<SectionName>('Home')
+  const [timeOfLastClick, setTimeOfLastClick] = useState(0) // we need to keep track of this to disable the observer temporarily when user clicks on a link
   return (
-    <ActiveSlice.Provider value={{ activeSection, setActiveSection }}>
+    <ActiveSlice.Provider
+      value={{
+        activeSection,
+        setActiveSection,
+        timeOfLastClick,
+        setTimeOfLastClick,
+      }}
+    >
       {children}
     </ActiveSlice.Provider>
   )
 }
 
 export default ActiveSectionContextProvider
-
