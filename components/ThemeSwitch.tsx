11176b06242b094948eb1ad9c17c71c905503a9d
Helmy Ezzat <hellmyezzat@gmail.com>
2024-02-20 23:42:41 +0200

    Dark mode ( part 2 )

--- components/ThemeSwitch.tsx
+++ components/ThemeSwitch.tsx
@@ -1,7 +1,10 @@
+'use client'
+import { useTheme } from '@/context/theme'
 import React from 'react'
-import { BsSun } from 'react-icons/bs'
+import { BsMoon, BsSun } from 'react-icons/bs'
 
 function ThemeSwitch() {
+  const { theme, toggleTheme } = useTheme()
   return (
     <button
       className="bg-white
@@ -9,8 +12,9 @@ function ThemeSwitch() {
     border border-white border-opacity-40 shadow-2xl rounded-full
     flex items-center justify-center
     hover:scale-[1.15] active:scale-105 transition-all"
+      onClick={toggleTheme}
     >
-      <BsSun />
+      {theme === 'light' ? <BsSun /> : <BsMoon />}
     </button>
   )
 }
--- context/theme.tsx
+++ context/theme.tsx
@@ -1,7 +1,6 @@
 'use client'
 import React, { createContext, useContext, useEffect, useState } from 'react'
 
-
 type Theme = 'light' | 'dark'
 type ThemeProps = {
   children: React.ReactNode
@@ -15,8 +14,29 @@ const ThemeSlice = createContext<ThemeType | null>(null)
 
 function ThemeContextProvider({ children }: ThemeProps) {
   const [theme, setTheme] = useState<Theme>('light')
-  const toggleTheme = () => {}
-  useEffect(() => {}, [])
+  const toggleTheme = () => {
+    if (theme === 'light') {
+      setTheme('dark')
+      localStorage.setItem('theme', 'dark')
+      document.documentElement.classList.add('dark')
+    } else {
+      setTheme('light')
+      localStorage.setItem('theme', 'light')
+      document.documentElement.classList.remove('dark')
+    }
+  }
+  useEffect(() => {
+    const localTheme = localStorage.getItem('theme') as Theme | null
+    if (localTheme) {
+      setTheme(localTheme)
+      if (localTheme === 'dark') {
+        document.documentElement.classList.add('dark')
+      }
+    } else if (window.matchMedia('(prefers-color-scheme: dark)').matches) {
+      setTheme('dark')
+      document.documentElement.classList.add('dark')
+    }
+  }, [])
 
   return (
     <ThemeSlice.Provider value={{ theme, toggleTheme }}>
